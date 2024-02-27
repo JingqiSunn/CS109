13e4617bad20baf0cc38dce2fb5c4c9857fc15e3
JingqiSunn <142304970+JingqiSunn@users.noreply.github.com>
2024-02-27 18:53:54 +0800

    Update
    I have finished the Exercise3OJVersion.

--- Lab Exercise/Week2/src/Exercise3OJVersion.java
+++ Lab Exercise/Week2/src/Exercise3OJVersion.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 public class Exercise3OJVersion {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int N, T, M, sum, s, difference;
-        sum = 0;
+        int N, T, M, sum, s, difference, n;
         System.out.print("The number of the numbers you are going to put in: ");
         N = input.nextInt();
         System.out.print("The number of times you want to take tests: ");
@@ -11,13 +10,15 @@ public class Exercise3OJVersion {
         System.out.print("The number you want to compared with: ");
         M = input.nextInt();
         while(T -- > 0 ) {
-            while (N-- > 0) {
+            n = N;
+            sum = 0;
+            while (n -- > 0) {
                 System.out.print("Please give me a number to add in: ");
                 s = input.nextInt();
                 sum += s;
             }
             difference = sum - M;
-            System.out.printf("The difference between the sum is %d ", difference);
+            System.out.printf("The difference between the sum is %d \n", difference);
         }
     }
 }
