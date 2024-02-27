aeb588b911578cd0d442f741f0cfd9f14d8d3e62
JingqiSunn <142304970+JingqiSunn@users.noreply.github.com>
2024-02-27 16:17:14 +0800

    Update
    I have finished and examine the exercise three for human users.

--- Lab Exercise/Week2/src/Exercise3.java
+++ Lab Exercise/Week2/src/Exercise3.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 public class Exercise3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int
+        int N, M, sum, s, difference;
+        sum = 0;
+        System.out.print("The number of the numbers you are going to put in: ");
+        N = input.nextInt();
+        System.out.print("The number you want to compared with: ");
+        M = input.nextInt();
+        while(N -- > 0 ) {
+            System.out.print("Please give me a number to add in: ");
+            s = input.nextInt();
+            sum += s;
+        }
+        difference = sum - M;
+        System.out.printf("The difference between the sum is %d ", difference);
     }
 }
