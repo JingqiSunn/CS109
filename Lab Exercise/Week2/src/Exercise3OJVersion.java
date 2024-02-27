1d393ebef2f93c60122336eb70eeb368d810ec3f
JingqiSunn <142304970+JingqiSunn@users.noreply.github.com>
2024-02-27 17:17:26 +0800

    Update
    Version for OJ test but not examined yet, and also didn't delete the reminding lines.

---
+++ Lab Exercise/Week2/src/Exercise3OJVersion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Exercise3OJVersion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N, T, M, sum, s, difference;
+        sum = 0;
+        System.out.print("The number of the numbers you are going to put in: ");
+        N = input.nextInt();
+        System.out.print("The number of times you want to take tests: ");
+        T = input.nextInt();
+        System.out.print("The number you want to compared with: ");
+        M = input.nextInt();
+        while(T -- > 0 ) {
+            while (N-- > 0) {
+                System.out.print("Please give me a number to add in: ");
+                s = input.nextInt();
+                sum += s;
+            }
+            difference = sum - M;
+            System.out.printf("The difference between the sum is %d ", difference);
+        }
+    }
+}
