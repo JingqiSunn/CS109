bc3002ecf5da78129cfe5775ea44c1902bbbadac
JingqiSunn <142304970+JingqiSunn@users.noreply.github.com>
2024-02-27 19:02:35 +0800

    Update
    I have finished Exercise3OJVersionWithoutNotationSentences.

---
+++ Lab Exercise/Week2/src/Exercise3OJVersionWithoutNotationSentences.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Exercise3OJVersionWithoutNotationSentences {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N, T, M, sum, s, difference, n;
+        N = input.nextInt();
+        T = input.nextInt();
+        M = input.nextInt();
+        while(T -- > 0 ) {
+            n = N;
+            sum = 0;
+            while (n -- > 0) {
+                s = input.nextInt();
+                sum += s;
+            }
+            difference = sum - M;
+            System.out.println(difference);
+        }
+    }
+}
