2e4fe2295d84aca6fce0ccae9c06538ed6c3728a
JingqiSunn <142304970+JingqiSunn@users.noreply.github.com>
2024-02-28 18:50:29 +0800

    Upload
    The program has the ability to count the all the number of critical below the upperbound

--- OJ Assignment/Assignment 1/src/ProblemOne.java
+++ OJ Assignment/Assignment 1/src/ProblemOne.java
@@ -3,28 +3,33 @@ import java.util.Scanner;
 public class ProblemOne {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int upperBound, lastNumber, remaining, uppersubTotal;
+        int upperBound, lastNumber, remaining, uppersubTotal, upCountDown;
         Boolean whetherCriticalPoint;
         whetherCriticalPoint = false;
         uppersubTotal = 0;
         System.out.print("Please give us the upperbound: ");
         upperBound = input.nextInt();
-        if (upperBound % 7 == 0) {
-            whetherCriticalPoint = true;
-        } else {
-            remaining = upperBound;
-            while (remaining > 0) {
-                lastNumber = remaining % 10;
-                remaining = remaining / 10;
-                if (lastNumber == 7) {
-                    whetherCriticalPoint = true;
+        upCountDown = upperBound;
+        while (upCountDown > 0) {
+            if (upCountDown % 7 == 0) {
+                whetherCriticalPoint = true;
+            } else {
+                remaining = upCountDown;
+                while (remaining > 0) {
+                    lastNumber = remaining % 10;
+                    remaining = remaining / 10;
+                    if (lastNumber == 7) {
+                        whetherCriticalPoint = true;
+                    }
                 }
             }
-        }
-        if (whetherCriticalPoint == true) {
-            uppersubTotal += 1;
-        } else {
-            uppersubTotal += 0;
+            if (whetherCriticalPoint == true) {
+                uppersubTotal += 1;
+            } else {
+                uppersubTotal += 0;
+            }
+            upCountDown -= 1;
+            whetherCriticalPoint = false;
         }
         System.out.printf("The total number of critical numbers are %d", uppersubTotal);
     }
