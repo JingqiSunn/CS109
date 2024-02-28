b3ff42fcd81af64f33e636b120026f43979d5ac5
JingqiSunn <142304970+JingqiSunn@users.noreply.github.com>
2024-02-28 19:29:52 +0800

    update
    The version for OJ test is ready

---
+++ OJ Assignment/Assignment 1/src/ProblemOneOJVersion.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class ProblemOneOJVersion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int upperBound, lastNumber, remaining, uppersubTotal, upCountDown, lowerbound, lowCountDown, lowersubTotal,
+                lowerDifference, testNumber;
+        Boolean whetherCriticalPoint;
+        whetherCriticalPoint = false;
+        System.out.print("How many tests do you want to give: ");
+        testNumber = input.nextInt();
+        while (testNumber-- > 0) {
+            uppersubTotal = 0;
+            lowersubTotal = 0;
+            System.out.print("Please give us the lowerbound: ");
+            lowerbound = input.nextInt();
+            System.out.print("Please give us the upperbound: ");
+            upperBound = input.nextInt();
+            upCountDown = upperBound;
+            lowCountDown = lowerbound - 1;
+            while (upCountDown > 0) {
+                if (upCountDown % 7 == 0) {
+                    whetherCriticalPoint = true;
+                } else {
+                    remaining = upCountDown;
+                    while (remaining > 0) {
+                        lastNumber = remaining % 10;
+                        remaining = remaining / 10;
+                        if (lastNumber == 7) {
+                            whetherCriticalPoint = true;
+                        }
+                    }
+                }
+                if (whetherCriticalPoint == true) {
+                    uppersubTotal += 1;
+                } else {
+                    uppersubTotal += 0;
+                }
+                upCountDown -= 1;
+                whetherCriticalPoint = false;
+            }
+            whetherCriticalPoint = false;
+            if (lowCountDown == -1) {
+                lowersubTotal = -1;
+            } else {
+                while (lowCountDown > 0) {
+                    if (lowCountDown % 7 == 0) {
+                        whetherCriticalPoint = true;
+                    } else {
+                        remaining = lowCountDown;
+                        while (remaining > 0) {
+                            lastNumber = remaining % 10;
+                            remaining = remaining / 10;
+                            if (lastNumber == 7) {
+                                whetherCriticalPoint = true;
+                            }
+                        }
+                    }
+                    if (whetherCriticalPoint == true) {
+                        lowersubTotal += 1;
+                    } else {
+                        lowersubTotal += 0;
+                    }
+                    lowCountDown -= 1;
+                    whetherCriticalPoint = false;
+                }
+            }
+            lowerDifference = uppersubTotal - lowersubTotal;
+            System.out.printf("The total number of critical numbers are %d", lowerDifference);
+        }
+    }
+}
