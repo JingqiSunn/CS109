ab63223bba7896277e95b3d60e424479c18a5fda
JingqiSunn <142304970+JingqiSunn@users.noreply.github.com>
2024-02-28 18:59:05 +0800

    update
    There is still problem with the bound to  be zero

--- OJ Assignment/Assignment 1/src/ProblemOne.java
+++ OJ Assignment/Assignment 1/src/ProblemOne.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class ProblemOne {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int upperBound, lastNumber, remaining, uppersubTotal, upCountDown;
+        int upperBound, lastNumber, remaining, uppersubTotal, upCountDown, lowerbound, lowCountDown, lowersubTotal, lowerDifference;
         Boolean whetherCriticalPoint;
         whetherCriticalPoint = false;
         uppersubTotal = 0;
+        lowersubTotal = 0;
+        System.out.print("Please give us the lowerbound: ");
+        lowerbound = input.nextInt();
         System.out.print("Please give us the upperbound: ");
         upperBound = input.nextInt();
         upCountDown = upperBound;
+        lowCountDown = lowerbound - 1;
         while (upCountDown > 0) {
             if (upCountDown % 7 == 0) {
                 whetherCriticalPoint = true;
@@ -31,6 +35,28 @@ public class ProblemOne {
             upCountDown -= 1;
             whetherCriticalPoint = false;
         }
+        while (lowCountDown > 0) {
+            if (lowCountDown % 7 == 0) {
+                whetherCriticalPoint = true;
+            } else {
+                remaining = lowCountDown;
+                while (remaining > 0) {
+                    lastNumber = remaining % 10;
+                    remaining = remaining / 10;
+                    if (lastNumber == 7) {
+                        whetherCriticalPoint = true;
+                    }
+                }
+            }
+            if (whetherCriticalPoint == true) {
+                lowersubTotal += 1;
+            } else {
+                lowersubTotal += 0;
+            }
+            upCountDown -= 1;
+            whetherCriticalPoint = false;
+        }
+        lowerDifference = uppersubTotal - lowersubTotal;
         System.out.printf("The total number of critical numbers are %d", uppersubTotal);
     }
 }
