bcc1b370003802912c3b8d9ea8b72a450cef35bb
JingqiSunn <142304970+JingqiSunn@users.noreply.github.com>
2024-02-28 14:57:20 +0800

    update
    Now the program can judge whether a number is a critical number

--- OJ Assignment/Assignment 1/src/ProblemOne.java
+++ OJ Assignment/Assignment 1/src/ProblemOne.java
@@ -3,24 +3,28 @@ import java.util.Scanner;
 public class ProblemOne {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int total, upperBound, lastNumber, remaining;
-        total = 0;
+        int upperBound, lastNumber, remaining, subTotal;
+        Boolean whetherCriticalPoint;
+        whetherCriticalPoint = false;
         System.out.print("Please give us the upperbound: ");
         upperBound = input.nextInt();
-        if(upperBound % 7 == 0) {
-            total += 1;
+        if (upperBound % 7 == 0) {
+            whetherCriticalPoint = true;
         } else {
             remaining = upperBound;
-            while(remaining > 0) {
+            while (remaining > 0) {
                 lastNumber = remaining % 10;
                 remaining = remaining / 10;
-                if (lastNumber == 7){
-                    total += 1;
-                } else {
-                    total = total;
+                if (lastNumber == 7) {
+                    whetherCriticalPoint = true;
                 }
             }
         }
-        System.out.printf("The total number of critical numbers are %d", total);
+        if (whetherCriticalPoint == true) {
+            subTotal = 1;
+        } else {
+            subTotal = 0;
+        }
+        System.out.printf("The total number of critical numbers are %d", subTotal);
     }
 }
