6fd7ebf85ae3dc33ab390757b9a52b5a990644e3
JingqiSunn <142304970+JingqiSunn@users.noreply.github.com>
2024-02-28 14:01:17 +0800

    update
    There is still some problems

--- OJ Assignment/Assignment 1/src/ProblemOne.java
+++ OJ Assignment/Assignment 1/src/ProblemOne.java
@@ -2,6 +2,25 @@ import java.util.Scanner;
 
 public class ProblemOne {
     public static void main(String[] args) {
-        Scanner
+        Scanner input = new Scanner(System.in);
+        int total, upperBound, lastNumber, remaining;
+        total = 0;
+        System.out.print("Please give us the upperbound: ");
+        upperBound = input.nextInt();
+        if(upperBound % 7 == 0) {
+            total += 1;
+        } else {
+            remaining = upperBound;
+            while(remaining > 0) {
+                lastNumber = remaining % 10;
+                remaining = remaining / 10;
+                if (lastNumber == 7){
+                    total += 1;
+                } else {
+                    total = total;
+                }
+            }
+        }
+        System.out.printf("The total number of critical numbers are %d", total);
     }
 }
