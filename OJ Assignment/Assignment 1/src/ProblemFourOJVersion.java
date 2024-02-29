fe46cc10680297708c6ebd01ad9ba287792f6511
JingqiSunn <142304970+JingqiSunn@users.noreply.github.com>
2024-02-29 18:52:33 +0800

    Update
    The problem four for OJ version is ready

---
+++ OJ Assignment/Assignment 1/src/ProblemFourOJVersion.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class ProblemFourOJVersion {
+    public static void main(String[] args) {
+        Scanner fetch = new Scanner(System.in);
+        int upperBound, lowerBound, upperNumber, lowerNumber, finalCriticalNumber, testTimes;
+        System.out.print("Give me your test times: ");
+        testTimes = fetch.nextInt();
+        while (testTimes-- > 0) {
+            System.out.print("Give me your lower bound: ");
+            lowerBound = fetch.nextInt();
+            System.out.print("Give me your upper bound: ");
+            upperBound = fetch.nextInt();
+            upperNumber = findNumberOfCriticalNumbersUnderABound(upperBound);
+            lowerNumber = findNumberOfCriticalNumbersUnderABound(lowerBound - 1);
+            finalCriticalNumber = upperNumber - lowerNumber;
+            System.out.print("The final number of critical numbers are " + finalCriticalNumber);
+        }
+    }
+
+
+    public static int findNumberOfCriticalNumbersUnderABound(int bound) {
+        int currentNumber, lastNumber, remainer, totalCritcalNumber;
+        boolean whetherCriticalNumber;
+        whetherCriticalNumber = false;
+        totalCritcalNumber = 0;
+        currentNumber = bound;
+        while (currentNumber >= 0) {
+            if (currentNumber % 7 == 0) {
+                whetherCriticalNumber = true;
+            } else {
+                remainer = currentNumber;
+                while (remainer > 0) {
+                    lastNumber = remainer % 10;
+                    remainer = remainer / 10;
+                    if (lastNumber == 7) {
+                        whetherCriticalNumber = true;
+                        break;
+                    }
+                }
+            }
+            if (whetherCriticalNumber == true) {
+                totalCritcalNumber += 1;
+            } else {
+                totalCritcalNumber += 0;
+            }
+            currentNumber -= 1;
+            whetherCriticalNumber = false;
+        }
+        return totalCritcalNumber;
+    }
+}
